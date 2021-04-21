03edfca564bc5956570210cc6666b1e56a15dc08
MiriamLL <miriamjlerma@gmail.com>
2021-04-21 17:44:02 +0200

    Inclui TDR_data en el paquete y la documentacion

---
+++ R/TDR_raw.R
@@ -0,0 +1,20 @@
+#' Raw data from a TDR (time and depth recorder) of a Humboldt penguin
+#'
+#' The data includes raw data obtained from a Humbolt Penguin at Tilgo, Chile
+#' The information was collected during the breeding season 2018.
+#' It contains 264197 obs from 1 variable.
+#'
+#' @docType data
+#'
+#' @usage data(TDR_raw)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#' @references Lerma et al. 2021
+#'
+#'
+#' @examples
+#' data(TDR_raw)
+"TDR_raw"
