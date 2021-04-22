12c84c127b589570fcb9e38be405e9f15f56722b
MiriamLL <miriamjlerma@gmail.com>
2021-04-22 14:25:32 +0200

    Agregar funciones: cut_trips, cut_rawdata Agregar datos: TDR_pressure

---
+++ R/TDR_pressure.R
@@ -0,0 +1,25 @@
+#' Pressure data from TDRS
+#'
+#' EN:
+#' The data includes pressure data obtained from a Humbolt Penguin at Tilgo, Chile
+#' The information was collected during the breeding season 2018.
+#' It contains 255600 obs from 3 variables: tStamp, Pressure, numeric_sequence.
+#' ES:
+#' Los datos incluyen datos crudos de un pinguino de Humbold muestreado en Isla Tilgo, Chile
+#' La information fue colectada durante la temporada reproductiva 2018.
+#' Contiene 255600 observaciones de 3 variables: tStamp, Pressure, numeric_sequence.
+#'
+#' @docType data
+#'
+#' @usage data(TDR_pressure)
+#'
+#' @format A rda data with one column and 255600 rows
+#'
+#' @keywords datasets
+#'
+#' @references Lerma et al. 2021
+#'
+#'
+#' @examples
+#' data(TDR_pressure)
+"TDR_pressure"
--- R/TDR_raw.R
+++ R/TDR_raw.R
@@ -1,8 +1,15 @@
-#' Raw data from a TDR (time and depth recorder) of a Humboldt penguin
+#' EN: Raw data from a time and depth recorder of a Humboldt penguin. ES: Datos crudos de TDR en Pinguinos de Humboldt
 #'
+#' EN:
 #' The data includes raw data obtained from a Humbolt Penguin at Tilgo, Chile
 #' The information was collected during the breeding season 2018.
 #' It contains 264197 obs from 1 variable.
+#' Data is provided as it is imported from the device.
+#' ES:
+#' Los datos incluyen datos crudos de un pinguino de Humbold muestreado en Isla Tilgo, Chile
+#' La information fue colectada durante la temporada reproductiva 2018.
+#' Contiene 264197 observaciones de 1 variable.
+#' Es un ejemplo de como los datos son descargados del dispositivo.
 #'
 #' @docType data
 #'
