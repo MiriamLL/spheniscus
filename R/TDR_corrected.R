526fc0e2b0d7cf457da3f8df118fce4c29ef5bc8
MiriamLL <miriamjlerma@gmail.com>
2021-04-26 19:38:09 +0200

    Agregar datos a paquete

---
+++ R/TDR_corrected.R
@@ -0,0 +1,25 @@
+#' Corrected data from TDRs
+#'
+#' EN:
+#' The data includes pressure data obtained from a Humbolt Penguin at Tilgo, Chile
+#' The information was collected during the breeding season 2018.
+#' It contains 42388 obs from 6 variables: tStamp, Pressure, numeric_sequence, daytime, depth and corrected_depth
+#' ES:
+#' Los datos incluyen datos crudos de un pinguino de Humbold muestreado en Isla Tilgo, Chile
+#' La information fue colectada durante la temporada reproductiva 2018.
+#' Contiene 42388 observaciones de 6 variables: tStamp, Pressure, numeric_sequence, daytime, depth and corrected_depth
+#'
+#' @docType data
+#'
+#' @usage data(TDR_corrected)
+#'
+#' @format A rda data with one column and 42388 rows
+#'
+#' @keywords datasets
+#'
+#' @references Lerma et al. 2021
+#'
+#'
+#' @examples
+#' data(TDR_corrected)
+"TDR_corrected"
---
+++ R/TDR_trip.R
@@ -0,0 +1,25 @@
+#' TDR_trip contains information only from when the bird was at sea
+#'
+#' EN:
+#' The data includes pressure data obtained from a Humbolt Penguin at Tilgo, Chile
+#' The information was collected during the breeding season 2018.
+#' It contains 42388 obs from 4 variables: tStamp, Pressure, numeric_sequence, daytime.
+#' ES:
+#' Los datos incluyen datos crudos de un pinguino de Humbold muestreado en Isla Tilgo, Chile
+#' La information fue colectada durante la temporada reproductiva 2018.
+#' Contiene 42388 observaciones de 4 variables: tStamp, Pressure, numeric_sequence y daytime.
+#'
+#' @docType data
+#'
+#' @usage data(TDR_trip)
+#'
+#' @format A rda data with one column and 255600 rows
+#'
+#' @keywords datasets
+#'
+#' @references Lerma et al. 2021
+#'
+#'
+#' @examples
+#' data(TDR_trip)
+"TDR_trip"
